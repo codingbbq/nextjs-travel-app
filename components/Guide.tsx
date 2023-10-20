419fa2b06d4a2f9a957601341946f094b3d5841e
codingbbq <codingbbq@gmail.com>
2023-10-20 15:06:39 +0530

    Guide section

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -1,7 +1,40 @@
+import Image from "next/image";
 const Guide = () => {
     return (
         <>
-            Guide
+            <section className="flexCenter flex-col">
+                <div className="padding-container max-container w-full pb-24">
+                    <Image src="/camp.svg" alt="Camp" width={50} height={50} />
+                    <p className="uppercase regular-18 -mt-1 mb-3 text-green-50">We are here for you</p>
+                    <div className="flex flex-wrap justify-between gap-5 lg:gap-10">
+                        <h2 className="bold-40 lg:bold-64 xl:max-w-[400px]">Guide You to Easy Path</h2>
+                        <p className="regular-16 text-gray-30 xl:max-w-[520px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quia tempora facere quod. Eius, veritatis porro. Accusamus expedita nostrum, doloremque dolorem molestias commodi ad quos reiciendis dolores nobis harum. Velit!
+                        Fugiat distinctio error, maiores ipsa, ducimus quas alias temporibus molestias sapiente voluptates iure unde. Consectetur, quae voluptatem expedita dicta omnis eius aliquid labore. A, provident explicabo sed obcaecati vel praesentium?</p>
+                    </div>
+                </div>
+
+                <div className="flexCenter max-container relative w-full">
+                    <Image src="/boat.png" alt="Boat" width={1440} height={580} className="w-full object-cover object-center 2xl:rounded-5xl" />
+
+                    <div className="absolute flex bg-white py-8 pl-5 pr-7 gap-3 rounded-3xl border shadow-md md:left-[5%] lg:top-20">
+                        <Image src="/meter.svg" alt="Meter" width={16} height={108} className="h-full w-auto" />
+                        <div className="flexBetween flex-col">
+                            <div className="flex w-full flex-col">
+                                <div className="flexBetween w-full">
+                                    <p className="regular-16 text-gray-20">Destination</p>
+                                    <p className="bold-16 text-green-50">48 min</p>
+                                </div>
+                                <p className="bold-20 mt-2">Aguas Calientes</p>
+                            </div>
+
+                            <div className="flex w-full flex-col">
+                                <p className="regular-16 text-gray-20">Start trek</p>
+                                <h4 className="bold-20 mt-2 whitespace-nowrap">Wonorejo Pasuraun</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </>
     )
 };
